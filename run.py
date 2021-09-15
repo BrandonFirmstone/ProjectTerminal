5bfab166d3abfaa0b544d84123cb538a3c22b7d7
BrandonFirmstone <brandonfirmstone@outlook.com>
2021-09-15 21:28:11 +0000

    Added and adjusted functions to correct functionality of system.

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 from colorama import Fore
 from csv import writer
 
-all_jobs = pd.read_csv("jobs.csv")
+
 STATUSES = {
     "1": "Pending",
     "2": "Ongoing",
@@ -113,17 +113,33 @@ class Job:
                 csv_writer = writer(write_obj)
                 csv_writer.writerow(new_job)
 
-class SearchAndDestroy:
+def view_due_jobs():
+    all_jobs = get_all_jobs()
+    now = datetime.datetime.now()
+    date_formatted = now.strftime("%d/%m/%Y")
+    pending_jobs = all_jobs[((all_jobs['Job Status'] == 'Pending') & (all_jobs['Job Status'] == 'Ongoing')) & (all_jobs['Due Date'] <= date_formatted)]
+    specified_tasks = all_jobs[all_jobs['Due Date'].str.match(date_formatted)]
+    print(" Tasks due or overdue \n")
+    print(f"{Fore.RED}{specified_tasks} \033[39m \n")
+    return_to_menu()
 
-    def print_all_jobs(self):
+def print_all_jobs():
+    all_jobs = get_all_jobs()
+    print('####################')
+    print('# PRINT ALL JOBS')
+    print('####################\n')
+    print(all_jobs.to_string())
+    return_to_menu()
 
-        all_jobs = pd.read_csv("jobs.csv")
-        print(all_jobs.to_string())
-        return_to_menu()
-
-    def print_specific_jobs(self, category):
-
-        all_jobs = pd.read_csv("jobs.csv")
+def print_specific_jobs():
+        all_jobs = get_all_jobs()
+        status = None
+        while status not in STATUSES:
+            print("Please select a status:\n")
+            for key in STATUSES:
+                print(key, ' - ', STATUSES[key])
+            status = input("\n").strip()
+        category = STATUSES[status]
         if category == "Pending" or category == "Ongoing" or category == "Complete":
             specified_tasks = all_jobs[all_jobs['Job Status'].str.match(
                 category)]
@@ -135,91 +151,40 @@ class SearchAndDestroy:
                 status_selector()
             else:
                 return_to_menu()
-
-    def delete_specific_job(self):
-
-        all_jobs = pd.read_csv("jobs.csv")
-        print(" Please see all jobs below:")
-        print(all_jobs.to_string())
-        print("\n Please select the row you want to delete using the number to the far left of the row.\n")
-        user_selection = input()
-        index_selected = int(user_selection)
-        if (all_jobs.index == user_selection).any() is False:
-            print(" Please select a valid index.")
-            main_menu()
-        else:
-            print(all_jobs.iloc[[index_selected]])
-            print(
-                " Is this the row that you would like to delete?\n)
-            user_selection = yes_no_questions()
-            if user_selection == "Yes":
-                print(" Deleting row...")
-                new_data = all_jobs.drop(index_selected)
-                new_data.to_csv("jobs.csv", index=False)
-                print(" Deleted row successfully")
-                all_jobs = pd.read_csv("jobs.csv")
-                print(all_jobs.to_string())
-                return_to_menu()
-            else:
-                print("Failed to delete row.")
-                return_to_menu()
-
-    def update_status(self):
-
-        all_jobs = pd.read_csv("jobs.csv")
-        print("Showing Pending and Ongoing tasks. Please note once a Job has gone to Complete it cannot be changed.\n")
-        print("Press enter to continue...\n")
-        user_selection = input()
-        not_completed_jobs = all_jobs[(
-            all_jobs['Job Status'] == 'Pending') + (all_jobs['Job Status'] == 'Ongoing')]
-        print(not_completed_jobs)
-        print("Press enter to continue...\n")
-        user_selection = input()
-        print("Please select a job you would like to update by using the index on the far left of each row.")
-        user_selection = input()
-        index_selected = int(user_selection)
-        if (all_jobs.index == user_selection).any() is False:
-            print("Please select a valid index.")
-            Jobs.update_status()
-        else:
-            print(all_jobs.loc[[index_selected]])
-            print("Is this the job you would like to update?\n")
-            user_selection = yes_no_questions()
-            if user_selection == "Yes":
-                row_to_update = all_jobs.iloc[[index_selected]]
-                print(row_to_update)
-                print()
-                while True:
-                    if "Pending" in row_to_update.values:
-                        print(
-                            "Would you like to update from Pending to Ongoing or Complete?\n")
-                        user_selection = input().capitalize()
-                        while True:
-                            if user_selection == "Ongoing" or user_selection == "Complete":
-                                print(f"Updating to {user_selection}...")
-                                all_jobs.set_value(
-                                    index_selected, "Job Status", user_selection)
-                                all_jobs.to_csv("jobs.csv", index=False)
-                                print("Changes made successfully")
-                                break
-                            else:
-                                print("Error, unexpected input. Please try again")
-                        break
-                    else:
-                        print("Update status to Complete?")
-                        user_selection = yes_no_questions()
-                        if user_selection == "Yes":
-                            print("Updating to complete..")
-                            all_jobs.set_value(
-                                index_selected, "Job Status", "Complete")
-                            all_jobs.to_csv("jobs.csv", index=False)
-                        else:
-                            print("Cancelling operation")
-                            break
-            else:
-                print("An exception has occurred")
-                jobs.update_status()
         return_to_menu()
+
+def delete_specific_job():
+    all_jobs = get_all_jobs()
+    print('####################')
+    print('# DELETE JOB')
+    print('####################\n')
+    index_selected = job_selection()
+    print(all_jobs.iloc[[index_selected]])
+    print(" Is this the row that you would like to delete?\n")
+    user_selection = yes_no_questions()
+    if user_selection == "Yes":
+        print(" Deleting row...")
+        new_data = all_jobs.drop(index_selected)
+        new_data.to_csv("jobs.csv", index=False)
+        print(" Deleted row successfully")
+        all_jobs = pd.read_csv("jobs.csv")
+        print(all_jobs.to_string())
+        return_to_menu()
+    else:
+        print("Failed to delete row.")
+        return_to_menu()
+
+def job_selection():
+    all_jobs = get_all_jobs()
+    print(" Please see all jobs below:")
+    print(all_jobs.to_string())
+    print("\n Please select the row you want to select using the number to the far left of the row.\n")
+    user_selection = input()
+    index_selected = int(user_selection)
+    if (all_jobs.index == user_selection).any() is False:
+        print(" Please select a valid index.")
+        job_selection()
+    return index_selected
 
 def return_to_menu():
     print("Press Enter to return to the main menu.")
@@ -228,6 +193,7 @@ def return_to_menu():
 
 
 def yes_no_questions():
+    user_selection = None
     while user_selection not in OPTIONS:
         print("Please select from the following options:")
         for key in OPTIONS:
@@ -235,22 +201,22 @@ def yes_no_questions():
         user_selection = input("\n").strip()
     return OPTIONS[user_selection]
 
-
-def status_selector():
-    jobs = Jobs()
-    print(" Please select one of the following: \n")
-    print(" Pending(1)   Ongoing(2)   Complete(3) \n")
-    status_selection = input()
-    options = ["Pending", "Ongoing", "Complete"]
-    status_selection = int(status_selection) - 1
-    jobs.print_specific_jobs(options[status_selection])
-
+def update_status():
+    job_index = job_selection()
+    user_selection = None
+    while user_selection not in STATUSES:
+        for key in STATUSES:
+            print(key, ' - ', STATUSES[key])
+        user_selection = input("What status would you like to select?\n").strip()
+    all_jobs.at[job_index, 'Job Status'] = STATUSES[user_selection]
+    all_jobs.to_csv('jobs.csv', index=False)
 
 def help_function():
 
     print(chr(27) + "[2J")
-    print("              H E L P              ")
-    print("-----------------------------------")
+    print("####################")
+    print("HELP MENU")
+    print("####################\n")
     print(" Project Terminal is a project management application.\n This software is designed to help you keep track of what tasks need to be completed.")
     print(" Occasionally you will be asked for input.\n Typically, the software is looking for either a number correlating to an option on the screen or for a keyword such as 'Pending'.")
     print(" On the main menu, the options presented to you will do the following:")
@@ -260,6 +226,9 @@ def help_function():
     print("\n 4 - Add a new Job. Here, you can follow the instructions to create a new Job. Please be aware that dates are in the UK format ie DD/MM/YYYY.")
     return_to_menu()
 
+def get_all_jobs():
+    all_jobs = pd.read_csv("jobs.csv")
+    return all_jobs
 
 def main_menu():
     MENUS = {
@@ -272,12 +241,11 @@ def main_menu():
         "7": "Update Status",
         "0": "exit",
     }
-    jobs = Jobs()
-    all_jobs = pd.read_csv("jobs.csv")
+    all_jobs = get_all_jobs()
     print(chr(27) + "[2J")
-    print("   P R O J E C T  T E R M I N A L  \n")
-    print("         M A I N   M E N U         ")
-    print("-----------------------------------")
+    print("########################################")
+    print("           M A I N   M E N U         ")
+    print("########################################")
     user_selection = None
     while user_selection not in MENUS:
         for key in MENUS:
@@ -286,29 +254,31 @@ def main_menu():
     if user_selection == 1 or user_selection == "1":
         help_function()
     elif user_selection == 2 or user_selection == "2":
-        status_selector()
+        
+        print_specific_jobs()
     elif user_selection == 3 or user_selection == "3":
-        jobs.print_all_jobs()
+        print_all_jobs()
     elif user_selection == 4 or user_selection == "4":
-        jobs.create_new_job()
+        new_job = Job()
+        new_job.to_csv()
     elif user_selection == 5 or user_selection == "5":
-        jobs.view_due_jobs()
+        view_due_jobs()
     elif user_selection == 6 or user_selection == "6":
-        jobs.delete_specific_job()
+        delete_specific_job()
     elif user_selection == 7 or user_selection == "7":
-        jobs.update_status()
-    elif user_selection == "exit":
-        print("Are you sure? Type 1 to return to the main menu, 2 to exit the program.")  # noqa
-        user_selection = input()
-        if user_selection == "1":
+        update_status()
+
+    elif user_selection == 0:
+        print("Are you sure you would like to exit?")  
+        user_selection = yes_no_questions()
+        if user_selection == "Yes":
             main_menu()
-        elif user_selection == "2":
+        else:
             print("Thank you for using Project Terminal! Goodbye!")
             sys.exit(0)
-        else:
-            print("Unexpected input, returning to main menu.")
-            main_menu()
 
-
+print("###############################\n")
+print("P R O J E C T \nT E R M I N A L\n")
+print("###############################")
 while True:
     main_menu()
